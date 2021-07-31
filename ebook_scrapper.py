41d415b6a181175245932040532e13d3d4997973
Ilias Paschalis <i.paschalis@pobuca.com>
2021-07-31 15:10:29 +0300

    init

---
+++ ebook_scrapper.py
@@ -0,0 +1,83 @@
+import ebooklib
+from ebooklib import epub
+from bs4 import BeautifulSoup
+import argparse
+import glob
+import os
+import re
+
+# this might vary in each book
+div_classes_to_include = ["p", re.compile("p-indent(\d)*")]
+
+
+def epub2thtml(epub_path):
+    """Extracts chapters from .epub file.
+    """
+    book = epub.read_epub(epub_path)
+    chapters = []
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            chapters.append(item.get_content())
+    return chapters
+
+
+def chap2text(chap):
+    """Extracts the text of a chapter.
+    """
+    output = ''
+    soup = BeautifulSoup(chap, 'html.parser')
+    text = [el.get_text() for el in soup.find_all("div", div_classes_to_include)]
+    for t in text:
+        output += f'{t}\n'
+    return output
+
+
+def clean_text(text):
+    """Final pass to clean and format the chapter text.
+    """
+    # remove trailing or leading whitesplaces
+    text = text.strip()
+
+    # for some reason this should be whitespace. NOTE: Might differ in other books.
+    text = re.sub(r"\r\n", " ", text)
+
+    # replace multiple whitespaces with one
+    text = re.sub(r"\n+", "\n", text)
+
+    # replace multiple whitesplaces with one
+    text = re.sub(r"[^\S\r\n]+", ' ', text)
+
+    # remove whitespaces around line breaks
+    text = re.sub(r"\s\n\s", "\n", text)
+
+    # replace some symbols with more conventional ones
+    text = re.sub(r"[“”]", "\"", text)
+    text = re.sub(r"’", "'", text)
+
+    return text
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", required=False, default="./input", help="Directory containing the .epub files.")
+    parser.add_argument("--out", "-o", required=False, default="./output", help="Output directory. If not spcified, output is writen to stdout.")
+    args = parser.parse_args()
+
+    # dictionary to hold the text per book (eg. {"book1": "...", "book2": "..."})
+    final_book_texts = {}
+
+    for file in sorted(glob.glob(os.path.join(args.input, "*.epub"))):
+        chapters = epub2thtml(file)
+        chapter_texts = [chap2text(ch) for ch in chapters]
+        clean_chapter_texts = [clean_text(ch) for ch in chapter_texts if ch != '']
+        final_book_texts[os.path.splitext(os.path.basename(file))[0]] = clean_chapter_texts
+
+    os.makedirs(args.out, exist_ok=True)
+
+    for book in final_book_texts.keys():
+        with open(os.path.join(args.out, f"{book}.txt"), "w+") as f:
+            for i, chapter in enumerate(final_book_texts[book]):
+                f.write(chapter)
+                # add chapter breaks
+                if i != len(final_book_texts[book]) - 1:
+                    f.write('\n***\n')
